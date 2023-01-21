8ddca4d111852eeb07e764fe4540bd98607b5017
Fernando Guillen <fguillen.mail@gmail.com>
2023-01-21 20:31:59 +0100

    Adding points to label

--- HUD.gd
+++ HUD.gd
@@ -1,8 +1,9 @@
 extends Control
 
+onready var label_points:Label = $CanvasLayer/Panel/LabelPoints
 
 func _ready():
 	var _i = Global.connect("points_changed", self, "on_points_changed")
 
 func on_points_changed(value):
-	print("points_changed", value)
+	label_points.set_points(value)
---
+++ LabelPoints.gd
@@ -0,0 +1,5 @@
+extends Label
+
+
+func set_points(points):
+	text = "%07d" % points
