b793e2632d8966604118424dc4be2d6fa75502d7
Fernando Guillen <fguillen.mail@gmail.com>
2023-01-27 13:27:45 +0100

    Trying to make Global to make all the connections

--- Global.gd
+++ Global.gd
@@ -24,5 +24,19 @@ func set_shields_full(value):
 
 func set_player(player):
 	self.player = player
-	set_shields(player.damage_manager.health)
-	set_shields_full(player.damage_manager.health)
+	set_shields(player.damage_manager.health - 1)
+	set_shields_full(player.damage_manager.health - 1)
+
+	player_dead = false
+
+	player.connect("dead", self, "on_player_dead")
+	player.connect("health_changed", self, "on_player_health_changed")
+
+
+func on_player_dead():
+	player_dead = true
+	SceneSwitcher.load_game_over()
+
+
+func on_player_health_changed(value):
+	set_shields_full(value)
--- Player.gd
+++ Player.gd
@@ -6,21 +6,7 @@ onready var ship_engine: = $ShipEngine
 func _ready():
 	Global.set_player(self)
 	ship_engine.setup(self)
-	connect("dead", self, "on_dead")
-	connect("health_changed", self, "on_health_changed")
-	Global.player_dead = false
 
 
 func get_class():
 	return "Player"
-
-
-func on_dead(_position) -> void:
-	print("Player dead")
-	Global.player_dead = true
-	SceneSwitcher.load_game_over()
-
-
-func on_health_changed(value) -> void:
-	print("Player health changed")
-	Global.set_shields_full(value)
