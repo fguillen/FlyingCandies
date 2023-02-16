90f2725470864df8c14fd022e61f66e6740e5e87
Fernando Guillen <fguillen.mail@gmail.com>
2023-02-16 19:05:07 +0100

    Switcher Scene with sound

--- SceneSwitcher.gd
+++ SceneSwitcher.gd
@@ -20,4 +20,4 @@ func change_scene(target: String) -> void:
 	$AnimationPlayer.play("FadeIn")
 	yield($AnimationPlayer, "animation_finished")
 	get_tree().change_scene(target)
-	$AnimationPlayer.play_backwards("FadeIn")
+	$AnimationPlayer.play("FadeOut")
