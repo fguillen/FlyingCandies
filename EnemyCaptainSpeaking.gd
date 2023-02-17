d6ea54f98e1f826e373b5f8b9bf79aed68fd2ead
Fernando Guillen <fguillen.mail@gmail.com>
2023-02-17 12:01:39 +0100

    Enemy discouraging advice

---
+++ EnemyCaptainSpeaking.gd
@@ -0,0 +1,85 @@
+class_name EnemyCaptainSpeaking
+extends Node2D
+
+const ENEMY_DISCOURAGING = [
+	"Your efforts are futile. Human.",
+	"Surrender or face destruction. Human.",
+	"You are no match for us. Human.",
+	"I suggest you turn back. Human",
+	"Your arrogance will be your downfall.",
+	"I have seen your future, and it is bleak.",
+	"Your destruction is inevitable. Human.",
+	"Your defeat is certain. Human.",
+	"Your journey ends here. Human.",
+	"Leave now or suffer the consequences.",
+	"You will not survive this encounter."
+]
+
+const PILOT_ANSWERS = [
+	"You're no match for me!",
+	"I'll crush your fleet!",
+	"I fear no aliens!",
+	"This ends now, alien!",
+	"I'll take you all down!",
+	"My ship is unstoppable!",
+	"I'll wipe you out!",
+	"I am the ultimate warrior!",
+	"I'll take on your whole armada!",
+	"You're about to meet your doom!",
+	"My courage knows no bounds!",
+	"I am the scourge of the galaxy!"
+]
+
+
+const ENEMY_ANSWERS = [
+	"Your fate is sealed.",
+	"You've made a fatal mistake.",
+	"You've sealed your own fate.",
+	"You'll regret your arrogance.",
+	"You'll pay for your actions.",
+	"You've signed your death warrant.",
+	"I warned you of the consequences.",
+	"You've made a grave error.",
+	"You'll suffer for your defiance.",
+	"You've made a fatal miscalculation.",
+	"You'll soon regret your decision.",
+	"You've invited your own destruction.",
+	"You'll be destroyed without mercy.",
+	"You'll face the consequences.",
+	"You'll meet your end here.",
+	"You'll regret challenging us."
+]
+
+onready var animation_player := $AnimationPlayer
+
+onready var label_enemy_discouraging := $Dialog/LabelEnemyDiscouraging
+onready var label_pilot_answer := $Dialog/LabelPilotAnswer
+onready var label_enemy_answer := $Dialog/LabelEnemyAnswer
+
+onready var pilot_ui := $Dialog/PilotUI
+
+var random = RandomNumberGenerator.new()
+
+
+func _init():
+	random.randomize()
+
+
+func _ready():
+	animation_player.play("Play")
+
+	label_enemy_discouraging.text_to_display = ENEMY_DISCOURAGING[random.randi_range(0, ENEMY_DISCOURAGING.size() - 1)]
+	label_pilot_answer.text_to_display = PILOT_ANSWERS[random.randi_range(0, PILOT_ANSWERS.size() - 1)]
+	label_enemy_answer.text_to_display = ENEMY_ANSWERS[random.randi_range(0, ENEMY_ANSWERS.size() - 1)]
+
+	pilot_ui.setup(Global.pilot)
+
+	perform()
+
+
+func perform() -> void:
+	animation_player.play("Play")
+
+
+func _on_AnimationPlayer_animation_finished(_anim_name:String):
+	queue_free()
--- MiniMap.gd
+++ MiniMap.gd
@@ -5,15 +5,15 @@ extends Control
 export var progress := 0.0
 
 
-onready var ini_position:float = $Ini.global_position.x
-onready var end_position:float = $End.global_position.x
+onready var ini_map_position:float = $Ini.global_position.x
+onready var end_map_position:float = $End.global_position.x
 onready var marker := $Marker
 
 var player
 var battleship
 
-var real_ini_position:float
-var real_end_position:float
+var ini_global_position := 0.0
+var end_global_position := 0.0
 
 
 func _ready():
@@ -22,22 +22,22 @@ func _ready():
 
 
 func _process(_delta):
-	if(real_ini_position != null and real_end_position != null):
+	if(ini_global_position != 0 and end_global_position != 0):
 		_calculate_progress()
-		marker.global_position.x = lerp(ini_position, end_position, progress)
+		marker.global_position.x = lerp(ini_map_position, end_map_position, progress)
 
 
 func on_player_set(player):
 	print("MiniMap.on_player_set")
 	self.player = player
-	real_ini_position = player.global_position.x
+	ini_global_position = player.global_position.x
 
 
 func on_battleship_set(battleship):
 	print("MiniMap.on_battlehip_set")
 	self.battleship = battleship
-	real_end_position = battleship.global_position.x
+	end_global_position = battleship.global_position.x
 
 
 func _calculate_progress():
-	progress = (player.global_position.x - real_ini_position) / (real_end_position - real_ini_position)
+	progress = (player.global_position.x - ini_global_position) / (end_global_position - ini_global_position)
