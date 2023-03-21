2105af0d3d7b6990128c92247194a678d43eeef2
Fernando Guillen <fguillen.mail@gmail.com>
2023-03-21 18:48:54 +0100

    Boss music fade in

--- Scenes/Game/GameScene.gd
+++ Scenes/Game/GameScene.gd
@@ -3,6 +3,12 @@ extends Node
 onready var audio_stream_player_game:AudioStreamPlayer = $AudioStreamPlayerGame
 onready var audio_stream_player_boss:AudioStreamPlayer = $AudioStreamPlayerBoss
 
+var boss_music_initial_volume_db:float
+
+
+func _ready():
+	boss_music_initial_volume_db = audio_stream_player_boss.volume_db
+
 
 func music_game_start():
 	audio_stream_player_game.play()
@@ -13,6 +19,18 @@ func music_game_stop():
 
 
 func music_boss_start():
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(
+		audio_stream_player_boss,
+		"volume_db",
+		-80,
+		boss_music_initial_volume_db,
+		6,
+		Tween.TRANS_LINEAR,
+		Tween.EASE_IN
+	)
+	tween.start()
 	audio_stream_player_boss.play()
 
 
